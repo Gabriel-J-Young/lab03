75581a565965a9df0b052a92839e7ee50ef15568
Gabriel-J-Young <gjyoung@calpoly.edu>
2021-10-27 22:56:33 -0700

    knn fast now

--- InduceC45/InduceC45/knn.py
+++ InduceC45/InduceC45/knn.py
@@ -2,6 +2,7 @@ import argparse
 import math
 import pandas as pd
 import numpy as np
+import time
 
 parser = argparse.ArgumentParser()
 
@@ -19,7 +20,7 @@ args = parser.parse_args()
 
 trainingSet = pd.read_csv(args.TrainingSetFile.name)
 
-class_label_index = -1
+class_label_index = 0
 class_label_name = trainingSet.columns.values[class_label_index]
 
 numeric_names = [] #list of numberic attributes
@@ -31,61 +32,78 @@ for col in trainingSet:
         else:
             cata_names.append(col)
 
-#distance_dict = {} #save computed dists
+print(numeric_names)
+print(class_label_name)
+
+def distances(df, x):
+ # df is a data frame
+ # x is a vector
+
+  data = np.array(df)
+
+  return np.sqrt(np.sum(((data - x)**2), axis =1))
 
 
-def knn(D, k, x):
+def knn(D, k, x, num_D):
     #D - dataset
     #k - number of nearest neighbors
     #x - point to classify
     #numeric - what attributes are numeric
     neighbors = [()] *(k+1) #leave one space for x
-    
-    dists_numeric = []
+    #t = time.time()
+    x_numeric_tmp = []
+    for name in numeric_names:
+        x_numeric_tmp.append(x[1][name])
+    x_numeric = np.array(x_numeric_tmp)
+
+
+    dists_numeric = distances(num_D, x_numeric)
+    #print("np: " + str(time.time()-t))
+
     dists_cata = []
     class_labels = []
 
     #print(D[numeric_names].values.astype(float))
+    #t = time.time()
+    #for d in num_D: #compute numerical dists
+    #    sum_squared_dist = 0
+    #    for attr_idx, attr_val in enumerate(numeric_names):
+    #        sum_squared_dist += (d[attr_idx] - float(x[1][attr_val]))**2
+    #    dists_numeric.append(math.sqrt(sum_squared_dist))
+    #print("num loop: " + str(time.time()-t))
 
-    for d in D[numeric_names].values.astype(float): #compute numerical dists
-        sum_squared_dist = 0
-        for attr_idx, attr_val in enumerate(numeric_names):
-            sum_squared_dist += (d[attr_idx] - float(x[1][attr_val]))**2
-        dists_numeric.append(math.sqrt(sum_squared_dist))
-    
-    for d in D[cata_names].iterrows(): #compute cata dists
-        sum_cata_dist = 0
-        for attr_val in cata_names:
-            sum_cata_dist += d[1][attr_val] == x[1][attr_val]  
-        dists_cata.append(sum_cata_dist)
+    if (len(cata_names) != 0):
+        for d in D[cata_names].iterrows(): #compute cata dists
+            sum_cata_dist = 0
+            for attr_val in cata_names:
+                sum_cata_dist += d[1][attr_val] == x[1][attr_val]  
+            dists_cata.append(sum_cata_dist)
 
-    
-
-    
     max_dist = np.array(dists_numeric).max()
 
     dists_norm = [] #filled with tuples, item, and distance
-    for d_num, d_cata, label in zip(dists_numeric, dists_cata, D[class_label_name].tolist()):
-        c_m = len(cata_names)
-        c_n = len(numeric_names)
-        dists_norm.append((label,(d_num/max_dist)*(c_n/(c_m+d_num) + (d_cata)*(c_m/(c_m+c_n)))))
+    if (len(cata_names) == 0):
+        t = time.time()
+        for d_num, label in zip(dists_numeric, D[class_label_name].tolist()):
+            c_n = len(numeric_names)
+            dists_norm.append((label,(d_num/max_dist)))
+        #print("zip loop: " + str(time.time()-t))
+    else:
+        for d_num, d_cata, label in zip(dists_numeric, dists_cata, D[class_label_name].tolist()):
+            c_m = len(cata_names)
+            c_n = len(numeric_names)
+            dists_norm.append((label,(d_num/max_dist)*(c_n/(c_m+d_num) + (d_cata)*(c_m/(c_m+c_n)))))
 
-    
     #print(dists_norm)
     tmp_min = min(dists_norm, key = lambda t: t[1]) #remove ele with smallest dist
     dists_norm.remove(tmp_min)
 
-    #print(x[1])
-    #dists_norm.sort(key=lambda x: x[1])
-    
-
     k_nearest = []
-    for d in dists_norm[1:k+1]:
+    for d in range(k):
         tmp_min = min(dists_norm, key = lambda t: t[1]) #remove ele with smallest dist
         dists_norm.remove(tmp_min)
         k_nearest.append(tmp_min[0])
 
-    #print(k_nearest)
     return max(set(k_nearest), key=k_nearest.count)
 
 def print_c_mat(mat):
@@ -131,13 +149,13 @@ def print_stats(confusion_list, trainingSet):
 
 D = trainingSet.iloc[:][2:]
 
+for num in numeric_names: #drop all rows with "?"
+    D = D[(D != "?").all(axis=1)]
+
 class_labels = []
+num_D = D[numeric_names].values.astype(float)
 for idx, row in enumerate(D.iterrows()):
-    class_labels.append((knn(D, args.k, row)))
+    class_labels.append((knn(D, args.k, row, num_D), row[1][class_label_name]))
     #print(idx)
 
 print_stats(class_labels, D)
-
-
-
-
